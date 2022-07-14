610147cc384ff161fbabb9a9ebfd22b743f82b67
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-07-14 16:14:33 -0500

    Export `fish_color` variables so that they behave more like universal variables

--- conf.d/dracula.fish
+++ conf.d/dracula.fish
@@ -11,40 +11,40 @@ set -l cyan 8be9fd
 set -l pink ff79c6
 
 # Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-set -g fish_color_cancel $red --reverse
-set -g fish_color_option $orange
+set -gx fish_color_normal $foreground
+set -gx fish_color_command $cyan
+set -gx fish_color_keyword $pink
+set -gx fish_color_quote $yellow
+set -gx fish_color_redirection $foreground
+set -gx fish_color_end $orange
+set -gx fish_color_error $red
+set -gx fish_color_param $purple
+set -gx fish_color_comment $comment
+set -gx fish_color_selection --background=$selection
+set -gx fish_color_search_match --background=$selection
+set -gx fish_color_operator $green
+set -gx fish_color_escape $pink
+set -gx fish_color_autosuggestion $comment
+set -gx fish_color_cancel $red --reverse
+set -gx fish_color_option $orange
 
 # Default Prompt Colors
-set -g fish_color_cwd $green
-set -g fish_color_host $purple
-set -g fish_color_host_remote $purple
-set -g fish_color_user $cyan
+set -gx fish_color_cwd $green
+set -gx fish_color_host $purple
+set -gx fish_color_host_remote $purple
+set -gx fish_color_user $cyan
 
 # Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_background
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-set -g fish_pager_color_selected_background --background=$selection
-set -g fish_pager_color_selected_prefix $cyan
-set -g fish_pager_color_selected_completion $foreground
-set -g fish_pager_color_selected_description $comment
-set -g fish_pager_color_secondary_background
-set -g fish_pager_color_secondary_prefix $cyan
-set -g fish_pager_color_secondary_completion $foreground
-set -g fish_pager_color_secondary_description $comment
+set -gx fish_pager_color_progress $comment
+set -gx fish_pager_color_background
+set -gx fish_pager_color_prefix $cyan
+set -gx fish_pager_color_completion $foreground
+set -gx fish_pager_color_description $comment
+set -gx fish_pager_color_selected_background --background=$selection
+set -gx fish_pager_color_selected_prefix $cyan
+set -gx fish_pager_color_selected_completion $foreground
+set -gx fish_pager_color_selected_description $comment
+set -gx fish_pager_color_secondary_background
+set -gx fish_pager_color_secondary_prefix $cyan
+set -gx fish_pager_color_secondary_completion $foreground
+set -gx fish_pager_color_secondary_description $comment
